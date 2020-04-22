36ee28247b6a97caf5d9b92020638e8d9b2dd449
Chenyu-Tsai <skynova@livemail.tw>
2020-04-22 21:55:42 +0800

    add MRR evaluation function

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,119 @@
 import argparse
-from utils import *
+from utils import explainability_compare
 from transformers import XLNetTokenizer, XLNetPreTrainedModel
+
+def calculate(model, dataloader, tokenizer):
+    total = len(dataloader)
+    entail_total = 0
+    entail_total_len = 0
+    neutral_total = 0
+    neutral_total_len = 0
+    contradict_total = 0
+    contradict_total_len = 0
+    
+    entail_correct = 0
+    entail_correct_len = 0
+    neutral_correct = 0
+    neutral_correct_len = 0
+    contradict_correct = 0
+    contradict_correct_len = 0
+    
+    entail_MRR_c = 0.
+    neutral_MRR_c = 0.
+    contradict_MRR_c = 0.
+    
+    entail_MRR_inc = 0.
+    neutral_MRR_inc = 0.
+    contradict_MRR_inc = 0.
+    
+    model.eval()
+    with torch.no_grad():
+        data_iterator = tqdm.tqdm(dataloader, desc='Iteration')
+        for data in data_iterator:
+            if next(model.parameters()).is_cuda:
+                data = [t.to("cuda:0") for t in data if t is not None]
+            # predict
+            tokens_tensors, segments_tensors, masks_tensors = data[:3]
+            sentence_a = data[4][0]
+            sentence_b = data[5][0]
+            eval_sentence = data[6][0]
+            outputs = model(input_ids=tokens_tensors, 
+                            token_type_ids=segments_tensors, 
+                            attention_mask=masks_tensors)
+            logits = outputs[0]
+            _, pred = torch.max(logits.data, 1)
+            
+            # divide 3 class
+            label = data[3]
+            MRR, length = explainability_compare(model, tokenizer, sentence_a, sentence_b, eval_sentence)
+
+            if label == torch.tensor([0]):
+                entail_total += 1
+                entail_total_len += length
+                if pred == label:
+                    entail_correct += 1
+                    entail_correct_len += length
+                    entail_MRR_c += MRR
+                else:
+                    entail_MRR_inc += MRR
+            elif label == torch.tensor([1]):
+                neutral_total += 1
+                neutral_total_len += length
+                if pred == label:
+                    neutral_correct += 1
+                    neutral_correct_len += length
+                    neutral_MRR_c += MRR
+                else:
+                    neutral_MRR_inc += MRR
+            else:
+                contradict_total += 1
+                contradict_total_len += length
+                if pred == label:
+                    contradict_correct += 1
+                    contradict_correct_len += length
+                    contradict_MRR_c += MRR
+                else:
+                    contradict_MRR_inc += MRR
+    
+    return {
+        'total':total,
+        'total_MRR':round((entail_MRR_c+entail_MRR_inc+
+                           neutral_MRR_c+neutral_MRR_inc+
+                           contradict_MRR_c+contradict_MRR_inc)/total, 4),
+        'total_acc':round((entail_correct+neutral_correct+contradict_correct)/total, 2),
+        'total_mean_len':round((entail_total_len+neutral_total_len+contradict_total_len)/total, 1),
+        'entail_total':entail_total,
+        'entail_acc':round(entail_correct/entail_total, 2),
+        'entail_mean_len':round(entail_total_len/entail_total, 1),
+        'entail_MRR':round((entail_MRR_c+entail_MRR_inc)/entail_total, 4),
+        'entail_correct':entail_correct,
+        'entail_correct_mean_len':round(entail_correct_len/entail_correct, 1),
+        'entail_MRR_c':round(entail_MRR_c/entail_correct, 4),
+        'entail_incorrect':entail_total-entail_correct,
+        'entail_incorrect_mean_len':round((entail_total_len-entail_correct_len)/(entail_total-entail_correct), 2),
+        'entail_MRR_inc':round(entail_MRR_inc/(entail_total-entail_correct), 4),
+        'neutral_total':neutral_total,
+        'neutral_acc':round(neutral_correct/neutral_total, 2),
+        'neutral_mean_len':round(neutral_total_len/neutral_total, 1),
+        'neutral_MRR':round((neutral_MRR_c+neutral_MRR_inc)/neutral_total, 4),
+        'neutral_correct':neutral_correct,
+        'neutral_correct_mean_len':round(neutral_correct_len/neutral_correct, 1),
+        'neutral_MRR_c':round(neutral_MRR_c/neutral_correct, 4),
+        'neutral_incorrect':neutral_total-neutral_correct,
+        'neutral_incorrect_mean_len':round((neutral_total_len-neutral_correct_len)/(neutral_total-neutral_correct), 2),
+        'neutral_MRR_inc':round(neutral_MRR_inc/(neutral_total-neutral_correct), 4),
+        'contradict_total':contradict_total,
+        'contradict_acc':round(contradict_correct/contradict_total, 2),
+        'contradict_mean_len':round(contradict_total_len/contradict_total, 1),
+        'contradict_MRR':round((contradict_MRR_c+contradict_MRR_inc)/contradict_total, 4),
+        'contradict_correct':contradict_correct,
+        'contradict_correct_mean_len':round(contradict_correct_len/contradict_correct, 1),
+        'contradict_MRR_c':round(contradict_MRR_c/contradict_correct, 4),
+        'contradict_incorrect':contradict_total-contradict_correct,
+        'contradict_incorrect_mean_len':round((contradict_total_len-contradict_correct_len)/(contradict_total-contradict_correct), 2),
+        'contradict_MRR_inc':round(contradict_MRR_inc/(contradict_total-contradict_correct), 4),
+    }
+    
 
 def main():
     parser = argparse.ArgumentParser()
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import torch
 import random
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import ConcatDataset
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 def format_special_chars(tokens):
     return [t.replace('Ġ', ' ').replace('▁', ' ').replace('</w>', '') for t in tokens]
@@ -75,7 +79,7 @@ def MRR_mean(pair_truth, pair_all, top_k, times):
     final = final/times
     return final
 
-def explainability_compare(model, tokenizer, sentence_a, sentence_b, test_sentence_a, test_sentence_b):
+def explainability_compare(model, tokenizer, sentence_a, sentence_b, test_sentence_a):
     """ Evaluating MRR between model and attention span"""
     inputs = tokenizer.encode_plus(sentence_a, sentence_b, return_tensors='pt', add_special_tokens=True)
     input_ids = inputs['input_ids'].cuda()
@@ -99,7 +103,7 @@ def explainability_compare(model, tokenizer, sentence_a, sentence_b, test_senten
     pair = sorted(pair, key=lambda pair: pair[2], reverse=True)
     pair = pair_without_score(pair)
     
-    test_inputs = tokenizer.encode_plus(test_sentence_a, test_sentence_b, return_tensors='pt', add_special_tokens=False)
+    test_inputs = tokenizer.encode_plus(test_sentence_a, sentence_b, return_tensors='pt', add_special_tokens=False)
     test_input_ids = test_inputs['input_ids']
     test_input_ids.squeeze()
     test_tokens = tokenizer.convert_ids_to_tokens(test_input_ids.squeeze().tolist())
@@ -113,3 +117,168 @@ def explainability_compare(model, tokenizer, sentence_a, sentence_b, test_senten
     test_pair = pair_match(test_sentence_a_tokens, test_sentence_b_tokens, attn_data=None)
 
     return MRR_calculate(test_pair, pair)
+
+""" Preprocessing Functions """
+
+
+
+""" Data Related Functions """
+
+class Dataset_multi(Dataset):
+    """ Multi label dataset"""
+
+    def __init__(self, mode, tokenizer):
+        assert mode in ["train", "test", "train_RTE", "test_RTE", "train_multi", "test_multi", "train_multi_500"]
+        self.mode = mode
+        self.df = pd.read_csv(mode + ".tsv", sep="\t").fillna("")
+        if self.mode == "train_multi":
+            self.df = self.df[['text_a', 'text_b', 'labels']]
+        self.len = len(self.df)
+        self.label_map = {'True': 0, 'False': 1}
+        self.tokenizer = tokenizer
+    
+    # return trainging data
+    def __getitem__(self, idx):
+        if self.mode in ["test", "train_RTE", "test_RTE", "test_multi", "test_2"]:
+            text_a, text_b = self.df.iloc[idx, :2].values
+            label_tensor = None
+        else:
+            text_a, text_b = self.df.iloc[idx, :2].values
+            label = self.df.iloc[idx, 2].replace('[', '')
+            label = label.replace(']','')
+            label = np.fromstring(label, dtype=int, sep=',')
+            #print(type(label.to_list()))
+            label_tensor = torch.tensor(label, dtype=torch.float)
+        
+        # sentence_a tokens
+        word_pieces = []
+        tokens_a = self.tokenizer.tokenize(text_a + '<SEP>')
+        word_pieces += tokens_a
+        len_a = len(tokens_a)
+        
+        # sentence_b tokens
+        tokens_b = self.tokenizer.tokenize(text_b + '<SEP><CLS>')
+        word_pieces += tokens_b
+        len_b = len(word_pieces) - len_a
+        
+        # 將 token 序列轉換成索引序列
+        ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
+        tokens_tensor = torch.tensor(ids)
+        
+        # 將第一句 token 位置設為 0，其他為 1 表示第二句
+        segments_tensor = torch.tensor([0] * len_a + [1] * (len_b-1) + [2], 
+                                        dtype=torch.long)
+        
+        return (tokens_tensor, segments_tensor, label_tensor)
+    
+    def __len__(self):
+        return self.len
+
+class Dataset_3Way(Dataset):
+    """ RTE 3way dataset """
+    
+    def __init__(self, mode, tokenizer):
+        assert mode in ["train_filtered", "test", "train"]
+        self.mode = mode
+        self.df = pd.read_csv(mode + ".tsv", sep="\t").fillna("")
+        self.len = len(self.df)
+        self.label_map = {'ENTAILMENT': 0, 'UNKNOWN': 1, 'CONTRADICTION': 2}
+        self.tokenizer = tokenizer
+        
+    
+    # 回傳訓練資料的 function
+    def __getitem__(self, idx):
+        if self.mode == "test":
+            text_a, text_b, label = self.df.iloc[idx, :].values
+            label_tensor = torch.tensor(label)
+        else:
+            text_a, text_b, label = self.df.iloc[idx, :].values
+            label_tensor = torch.tensor(label)
+            
+        # 第一句 tokens
+        word_pieces = []
+        tokens_a = self.tokenizer.tokenize(text_a + '<SEP>')
+        word_pieces += tokens_a
+        len_a = len(tokens_a)
+        
+        # 第二句 tokens
+        tokens_b = self.tokenizer.tokenize(text_b + '<SEP><CLS>')
+        word_pieces += tokens_b
+        len_b = len(word_pieces) - len_a
+        
+        # 將 token 序列轉換成索引序列
+        ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
+        tokens_tensor = torch.tensor(ids)
+        
+        # 將第一句 token 位置設為 0，其他為 1 表示第二句
+        segments_tensor = torch.tensor([0] * len_a + [1] * (len_b-1) + [2], 
+                                        dtype=torch.long)
+        
+        return (tokens_tensor, segments_tensor, label_tensor)
+    
+    def __len__(self):
+        return self.len
+
+class Dataset_MRR(Dataset):
+    """ RTE 3way dataset """
+    
+    def __init__(self, mode, tokenizer):
+        assert mode in ["test_MRR"]
+        self.mode = mode
+        self.df = pd.read_csv(mode + ".tsv", sep="\t").fillna("")
+        self.len = len(self.df)
+        self.tokenizer = tokenizer
+        
+    def __getitem__(self, idx):
+        text_a, text_b, text_eval, label, t3, t5, t7 = self.df.iloc[idx, :].values
+        label_tensor = torch.tensor(label)
+            
+        # sentence_a tokens
+        word_pieces = []
+        tokens_a = self.tokenizer.tokenize(text_a + '<SEP>')
+        word_pieces += tokens_a
+        len_a = len(tokens_a)
+        
+        # sentence_b tokens
+        tokens_b = self.tokenizer.tokenize(text_b + '<SEP><CLS>')
+        word_pieces += tokens_b
+        len_b = len(word_pieces) - len_a
+        
+        # 將 token 序列轉換成索引序列
+        ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
+        tokens_tensor = torch.tensor(ids)
+        
+        # 將第一句 token 位置設為 0，其他為 1 表示第二句
+        segments_tensor = torch.tensor([0] * len_a + [1] * (len_b-1) + [2], 
+                                        dtype=torch.long)
+        
+        return (tokens_tensor, segments_tensor, label_tensor, text_a, text_b, text_eval)
+    
+    def __len__(self):
+        return self.len
+
+def MRR_mini_batch(samples):
+    tokens_tensors = [s[0] for s in samples]
+    segments_tensors = [s[1] for s in samples]
+    text_a = [s[3] for s in samples]
+    text_b = [s[4] for s in samples]
+    
+    if samples[0][2] is not None:
+        label_ids = torch.stack([s[2] for s in samples])
+    else:
+        label_ids = None
+    
+    # zero pad 到同一序列長度
+    tokens_tensors = pad_sequence(tokens_tensors, 
+                                  batch_first=True)
+    segments_tensors = pad_sequence(segments_tensors, 
+                                    batch_first=True)
+    
+    # attention masks，將 tokens_tensors 裡頭不為 zero padding
+    # 的位置設為 1，讓 model 只關注這些位置的 tokens
+    masks_tensors = torch.zeros(tokens_tensors.shape, 
+                                dtype=torch.long)
+    masks_tensors = masks_tensors.masked_fill(
+        tokens_tensors != 0, 1)
+    
+    return tokens_tensors, segments_tensors, masks_tensors, label_ids, text_a, text_b
